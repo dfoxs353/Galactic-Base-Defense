afea155abd55ea4469c39aad77b992a1e3b971bc
dfoxs353 <platoleg37@gmail.com>
2022-12-20 02:14:02 +0300

    Pull

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager _instance;
+    public static GameManager Instance{get {return _instance;}}
+
+    public Menu_Manager MenuManager;
+    public HUD_Manager HUDManager;
+
+    private string scene = "main";
     // Start is called before the first frame update
     void Start()
     {
+        if (Instance == null)
+        {
+            _instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;;
+        }
         
+        DontDestroyOnLoad(gameObject);
+
+        InitializeMain();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void InitializeMain()
+    {
+        MenuManager = GetComponent<Menu_Manager>();
+        HUDManager = GetComponent<HUD_Manager>();
     }
 }
--- Assets/Scripts/HUD_Manager.cs
+++ Assets/Scripts/HUD_Manager.cs
@@ -1,18 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HUD_Manager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void MenuPressed()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Menu pressed");
     }
 }
